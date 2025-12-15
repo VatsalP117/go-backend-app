123e343951c761e25bc79a5121aa32f79d42fbf1
Vatsal Patel <vatsal4011@gmail.com>
2025-12-15 23:54:07 +0530

    base server init

---
+++ internal/server/server.go
@@ -0,0 +1,60 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/VatsalP117/go-backend-app/internal/config"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/rs/zerolog/log"
+)
+
+// This file does three things:
+
+// Injects Dependencies: It takes your Config and stores it.
+
+// Initializes Echo: It sets up the web server instance.
+
+// Configures Middleware: It adds the standard "must-have" middleware (Logging, Recover, CORS).
+
+type Server struct {
+	Echo *echo.Echo
+	Config *config.Config
+}
+
+func NewServer(cfg *config.Config) *Server {
+	e := echo.New()
+
+	// 1. HIde the startup banner (so we have cleaner logs)
+	e.HideBanner = true
+	e.HidePort = true
+
+	// 2. Add Essential Middleware
+	// Recover: If your app crashes (panics), this catches it and keeps the server running
+	e.Use(middleware.Recover())
+
+	// Logger: Logs every request
+	e.Use(middleware.Logger())
+
+	// CORS: Allows requests from any origin (useful for development)
+	e.Use(middleware.CORSWithConfig(
+		middleware.CORSConfig{
+			AllowOrigins: []string{"http://localhost:3000"},
+			AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
+	}))
+
+	// 3. Define a simple health check route so we can test our server
+	e.GET("/health",func(c echo.Context) error {
+		return c.JSON(http.StatusOK,map[string]string{"status":"OK"})
+	})
+
+	return &Server{
+		Echo: e,
+		Config: cfg,
+	}
+}
+
+func (s *Server) Start() error{
+	log.Info().Msgf("Starting server on port %s", s.Config.Port)
+	return s.Echo.Start(":" + s.Config.Port)
+}
