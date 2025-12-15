a22ed725c0a4b8e01c35cbf5f1f590f942d277f9
Vatsal Patel <vatsal4011@gmail.com>
2025-12-15 23:39:48 +0530

    first config setup

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/VatsalP117/go-backend-app
+
+go 1.25.3
+
+require github.com/joho/godotenv v1.5.1 // indirect
---
+++ internal/config/config.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Port string
+	ClerkSecretKey string
+}
+
+func Load() *Config {
+	// 1. Try to load the .env file
+	// In production (like on a real server), this file might not exist 
+	// because you set actual environment variables. So we don't panic here.
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found, using environment variables directly")
+	}
+
+	// 2. Read and populate the struct
+	cfg := &Config{
+		Port: getEnv("PORT", "8080"),// Default to 8080 if PORT is not set
+		ClerkSecretKey: getEnv("CLERK_SECRET_KEY",""),
+	}
+
+
+	// 3. Validation
+	// Crash the app if critical keys are missing
+	if cfg.ClerkSecretKey == "" {
+		log.Fatal("Error: CLERK_SECRET_KEY is required but not set")
+	}
+
+	return cfg
+}
+
+
+func getEnv(key, fallback string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return fallback
+}
